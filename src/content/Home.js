0d6612e5ff2896f983f33a735490f2edd5b6b0b2
saivangapally <70342691+saivangapally@users.noreply.github.com>
2021-03-03 09:50:46 -0600

    changes

--- src/content/Home.js
+++ src/content/Home.js
@@ -4,14 +4,14 @@ class Home extends Component{
     render(){
         return(
             <div className="Bottomrightside">
-                <h1>Hey there, Welcome! </h1>
-                <p>I'm an Data Analytics professional with more than 3 years of Industry experience. I have Master's in Engineering 
-                    with experience of data processing, software programming, ETL, analysis and visualization.
+                <h1>Hello, Welcome! </h1>
+                <p>I'm a Data Engineering professional with more than 4 years of Industry experience. I have Master's in Engineering 
+                    with experience of bulding data pipelines, software programming, machine learning and analytics. I enjoy problem solving, working with data 
+                    and would love to build scalable data systems.
                 </p>
-                <p>My areas of interest include Data Science, Classical Machine Learning, Bayesian Machine Learning, Artificial Neural Networks.
-                    I'm passionate about learning and applying Data Science/Machine Learning to solve real world problems.
-                </p>
-                <p>I'm planning to use this platform to share my understanding of different concepts as blog posts, my goal is to learn and share my knowledge with the community. 
+                <p>
+                   I'm passionate about learning and applying technologies to solve complex problems to help organizations take data-driven decisions.
+                   I'm planning to use this platform to share my understanding of different concepts as blog posts, my goal is to learn and share my knowledge with the community. 
                 </p>
                 <h2>Contact</h2>
                 <p>If you would like to get in touch with me, you can directly reach 
