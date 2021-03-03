9ee72426a43de121861f53744ecc713622c96444
saivangapally <70342691+saivangapally@users.noreply.github.com>
2021-03-03 09:48:50 -0600

    changes

--- src/content/About.js
+++ src/content/About.js
@@ -7,19 +7,11 @@ class About extends Component{
             <div className = "Bottomrightside">
                 <h1>Education and Experience</h1>
                 <p>
-                I studied Master's in Mechanical Engineering where I got introduced to Artificial Neural Networks and Statistical tools which helped me to pursue my career in 
-                in Analytics and my goal is to become Machine Learning Practioner. 
-                My research at university includes process-parameter optimization study of additive manufacturing technique for fabricating bone scaffolds. 
+                I studied Master's in Engineering where I have gained the fundamental knowledge of various subjects including Data Analytics,Artificial Neural Networks, Additive Manufacturing, Process Improvement. 
+                After graduation,I have started working full time where I'm primarly responsible for building data pipelines for print processing and analytics. 
                 </p>
 
-
-                <p>
-                After my Master's, I joined Shutterfly as full-time employee where I worked on problems involving 
-                the automation of data pipelines, ETL, process improvement, software programming, data analysis, visualization and Machine Learning POCs. As part of ML work, me and my team-mate patented
-                the process of information extraction from documents using Machine Learning Techniques.
-                </p>
-
-                <h2>Tools and Skills</h2>
+                <h2>Software Tools and Skills</h2>
                 <p>During my work, I'm involved in wide range of projects which gave me opportunity to work with different tools and techniques.
                 {/* <ul>
                         <li>Computer Languages: SQL, Python, Java, Javascript, MATLAB</li>
@@ -27,18 +19,18 @@ class About extends Component{
                 </ul> */}
             </p>
             <ul className="ulclass">
-                    <li>Exploratory Data Analysis</li>
-                    <li>Machine Learning</li>
-                    <li>Statistics</li>
-                    <li>Software Programming</li>
-                    <li>SQL</li>
                     <li>Python</li>
+                    <li>SQL</li>
                     <li>Java</li>
-                    <li>Javascript</li>
-                    <li>Tableau</li>
-                    <li>Tensorflow</li>
+                    <li>JavaScript</li>
                     <li>Apache Spark</li>
-                    <li>MATLAB</li>
+                    <li>Hive </li>
+                    <li>Kafka</li>
+                    <li>Tableau</li>
+                    <li>Machine Learning</li>
+                    <li>Tensorflow</li>
+                    <li>Statistics</li>
+                    <li>Exploratory Analysis</li>
                 </ul>
             </div>
         )
